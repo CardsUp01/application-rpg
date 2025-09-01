eaf063c5dd943f682957748dd8ff324da2de4dc5
Mehmet Salih Kuscu <37173211+MehmetSalihK@users.noreply.github.com>
2025-09-01 16:03:48 +0200

    archer fait

--- src/com/activision/entity/Archer.java
+++ src/com/activision/entity/Archer.java
@@ -1,49 +1,50 @@
 package com.activision.entity;
 
-public class Archer extends Personnage{
+public class Archer extends Personnage {
 	
-	// attributs de l'archer
+	// Attributs de l'archer
 	private int fleches;
 	
-	// hp est compris 0 et 25
-
+	// Constructeurs
 	public Archer() {
 		super();
-		
 	}
 
 	public Archer(String nomRempli, int hpRempli, int levelRempli) {
 		super(nomRempli, hpRempli, levelRempli);
-		// spécialisation du personnage de type Archer
-		//this.fleches = fleches;
 	}
 	
+	// Getters et Setters
+	public int getFleches() {
+		return fleches;
+	}
+
+	public void setFleches(int fleches) {
+		this.fleches = fleches;
+	}
 	
+	// Méthodes
 	@Override
 	public void setHp(int hp) {
-		if(hp>=0 && hp<=40) {
-		      super.setHp(hp);
-		}else {
+		if (hp >= 0 && hp <= 40) {
+			super.setHp(hp);
+		} else {
 			throw new IllegalArgumentException("la valeur de hp n'est pas valide");
 		}
 	}
 
 	@Override
 	public void attaquer(Personnage ennemi) {
-		ennemi.setHp(ennemi.getHp()-3);
-		
+		ennemi.setHp(ennemi.getHp() - 3);
 	}
 
 	@Override
 	public void defendre() {
-		// TODO Auto-generated method stub
-		
+		// TODO: Implémenter la défense
 	}
 	
-	//création d'une méthode sans passer par l'instance qui fait combattre un personnage contre autre
-	
+	// Méthode statique de combat
 	public static void combat(Personnage p1, Personnage p2) {
 		p1.attaquer(p2);
 	}
-
 }
--- src/com/activision/test/TestPersonnage.java
+++ src/com/activision/test/TestPersonnage.java
@@ -1,15 +1,32 @@
 package com.activision.test;
 
-import com.activision.entity.Personnage;
+import com.activision.entity.Archer;
+import com.activision.entity.Voleur;
 
 public class TestPersonnage {
 
 	public static void main(String[] args) {
-		// création d'une instance de la classe Personnage
+		// Test de la classe Archer
+		System.out.println("=== Test Archer ===");
 		
+		// Création d'un archer
+		Archer legolas = new Archer("Legolas", 35, 10);
+		System.out.println("Archer créé: " + legolas);
 		
-		Personnage p1 = new Personnage();
-
+		// Test des flèches
+		legolas.setFleches(50);
+		System.out.println("Nombre de flèches: " + legolas.getFleches());
+		
+		// Test de combat
+		Voleur robin = new Voleur("Robin", 30, 8);
+		System.out.println("\nAvant combat:");
+		System.out.println("Legolas HP: " + legolas.getHp());
+		System.out.println("Robin HP: " + robin.getHp());
+		
+		// Combat avec méthode statique
+		Archer.combat(legolas, robin);
+		System.out.println("\nAprès combat:");
+		System.out.println("Robin HP: " + robin.getHp());
 	}
 
 }
